6196c8dec4a75f0d061613d997aa2c0116cbce18
Dovile <43847677+dovii@users.noreply.github.com>
2021-05-16 19:35:41 +0200

    initial setup and integration with google sheets

--- survey.js
+++ survey.js
@@ -26,16 +26,45 @@ var surveyjson = { "completedHtml": "Thank you for your participation.",
                 "type": "matrix", "name": "I was free to decide how I wanted to play.", "valueName": "Q1", "isRequired": true, "columns": ["Strongy disagree", "Disagree", "Neutral", "Agree", "Strongly agree"] }, {
                 "type": "matrix", "name": "I was free to decide how I wanted to play.", "valueName": "Q1", "isRequired": true, "columns": ["Strongy disagree", "Disagree", "Neutral", "Agree", "Strongly agree"] }, {
                 "type": "matrix", "name": "I was free to decide how I wanted to play.", "valueName": "Q1", "isRequired": true, "columns": ["Strongy disagree", "Disagree", "Neutral", "Agree", "Strongly agree"] }, {
-                 */}] }] }
+                 */
+                    "choicesByUrl": {
+                        //Here paste your own app URL
+                        "url": "https://script.google.com/macros/s/AKfycbxHYVECU8YE1TW3kU4lV6J4WKEeKPZG0Rd2uqkaVLm1cqjiAaeyJ6YIfgIZFCkWz8PE/exec",
+                        "path": "question1",
+                        "valueName": "value",
+                        "titleName": "title"
+                    },
+                    "hasNone": true
+                
+                
+                }] }] };
 
-window.survey = new Survey.Model(surveyjson);
-
-survey
-    .onComplete
-    .add(function (result) {
-        document
-            .querySelector('#surveyResult')
-            .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
+function sendDataToServer(survey, options) {
+    //Display information about sending data
+    options.showDataSaving();
+    $.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbxHYVECU8YE1TW3kU4lV6J4WKEeKPZG0Rd2uqkaVLm1cqjiAaeyJ6YIfgIZFCkWz8PE/exec',
+        type: 'post',
+        data: JSON.stringify(survey.data),
+        //We need tell web app that we use plain text.
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        processData: false,
+        complete: function (res, status) {
+            if (status == 'success') {
+                //Show that data was send successfully
+                options.showDataSavingSuccess();
+            } else {
+                //Display retry button in case of error
+                options.showDataSavingError();
+            }
+        },
     });
+}
 
-$("#surveyElement").Survey({ model: survey });
+var survey = new Survey.Model(surveyJSON);
+$("#surveyContainer").Survey({
+    model: survey,
+    onComplete: sendDataToServer
+});
