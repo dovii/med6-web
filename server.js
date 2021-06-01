c9d8ec9cd465f0005665e3ce14b91fde7ae62f61
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-01 09:20:01 +0200

    no message

--- server.js
+++ server.js
@@ -1,80 +1,52 @@
-// create an express app
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-const { MongoClient } = require("mongodb");
+const connectionString = 'mongodb+srv://sample-user:twsm@wow-web.pi0rs.mongodb.net/wow-survey?retryWrites=true&w=majority';
 
-const uri = process.env.MONGODB_URI;
+app.use(express.static("/"));
 
-// use the express-static middleware
-app.use(express.static("/public"));
-
-// define the first route
-app.get("/index.html", async function (req, res) {
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  
-  try {
-    await client.connect();
-
-    const database = client.db('sample_mflix');
-    const collection = database.collection('movies');
-
-    await listDatabases(client);
-
-    await createListing(client,
-      {
-        name: "Lovely Loft",
-        summary: "A charming loft in Paris",
-        bedrooms: 1,
-        bathrooms: 1
-      }
-    );
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { genres: "Comedy", poster: { $exists: true } };
-    const cursor = await collection.aggregate([
-      { $match: query },
-      { $sample: { size: 1 } },
-      { $project: 
-        {
-          title: 1,
-          fullplot: 1,
-          poster: 1
-        }
-      }
-    ]);
-
-    const result = await client.db("wow-survey").collection("results").insertOne(newListing)
-
-    //const movie = await cursor.next();
-
-    return res.json(result);
-  } catch(err) {
-    console.log(err);
-  }
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-
-  async function listDatabases(client) {
-    databasesList = await client.db().admin().listDatabases();
-
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-  };
-
-  async function createListing(client, newListing) {
-
-    const result = await client.db("wow-survey").collection("results").insertOne(newListing);
-
-    console.log(`New listing created with the following id: ${result.insertedId}`);
-
-  }
+const connectionParams = {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true
+}
 
 
+/*MongoClient.connect(connectionString, connectionParams, (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+}) */
+
+MongoClient.connect(connectionString, connectionParams)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('wow-survey')
+        const quotesCollection = db.collection('results')
+
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
 });
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/', (request, response) => {
+  quotesCollection.insertOne(request.body)
+    .then(result => {
+      response.redirect('/')
+      
+    })
+    .catch(error => console.error(error))
+})
+
+    })
