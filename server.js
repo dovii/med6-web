f1102bb319f1277cc892800927557522ee0e13bf
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-01 10:01:53 +0200

    no message

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ MongoClient.connect(connectionString, connectionParams)
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('wow-survey')
-        const quotesCollection = db.collection('results')
+        const resultsCollection = db.collection('results')
 
 
 // Make sure you place body-parser before your CRUD handlers!
@@ -33,22 +33,20 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(process.env.PORT || 5000,
     () => console.log("Server is running..."));
 
-/*app.listen(3000, function () {
-    console.log('listening on 3000')
-});*/
 
 
 app.get('/', function (request, response) {
     response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/survey.js');
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 })
 
 app.post('/', (request, response) => {
-  quotesCollection.insertOne(request.body)
+  resultsCollection.insertOne(request.body)
     .then(result => {
       response.redirect('/')
-      
+      console.log(`New listing created with the following id: ${result.insertedId}`);      
     })
     .catch(error => console.error(error))
 })
