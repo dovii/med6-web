571963e9e882c13a69439ef43e248df4dcf9f259
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-01 16:43:13 +0200

    installed http and socket.io

--- server.js
+++ server.js
@@ -10,26 +10,14 @@ app.use(express.static("/"));
 
 app.use(cors()); //Must be before BodyParser**
 
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
-
-
 
 
 const connectionParams = {
