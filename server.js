19ca82ba6fbeeb391ba99d819568bcac60b57155
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-03 19:22:43 +0200

    adjusting cors

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(cors()); //Must be before BodyParser**
 
 app.options('*', cors());
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 
 /*const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
