f3869e77ebe4ef4daa7caa1f9be8b2b7d8f35354
Dovile <43847677+dovii@users.noreply.github.com>
2021-06-08 19:48:58 +0200

    no message

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const connectionString = process.env.MONGODB_URI;
 
 app.use(express.static("/"));
 
-app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
